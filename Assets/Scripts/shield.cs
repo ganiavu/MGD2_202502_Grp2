11891eac619578bc8c3905f4e98c36a1d1cecd23
ganiavu <loke12necro@gmail.com>
2025-05-07 11:45:10 +0800

    trying on shield

---
+++ Assets/Scripts/shield.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public Collider playerCollider; // Assign this in the inspector
+
+    private bool shieldActive = false;
+
+    void OnEnable()
+    {
+        Debug.Log("Shield activated: Disabling player collider.");
+        if (playerCollider != null)
+        {
+            playerCollider.enabled = false;
+            shieldActive = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Shield hit something: " + other.name + " | Tag: " + other.tag);
+
+        if (shieldActive && other.CompareTag("Obstacle"))
+        {
+            Debug.Log("Hit Obstacle! Re-enabling player collider and disabling shield.");
+
+            if (playerCollider != null)
+            {
+                playerCollider.enabled = true;
+            }
+
+            gameObject.SetActive(false); // This should disable the shield
+        }
+    }
+
+}
